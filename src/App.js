a79c30042a4e62dba1031cb04453699aa65f66f6
Brad Herr <herrbrad@yahoo.com>
2022-04-11 12:07:33 -0400

    component skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video autoPlay loop muted id='video-background'>
+        <source src='https://www.youtube.com/watch?v=Pnxyh3_0tE4' type='video/mp4' />
+      </video>
     </div>
   );
 }
---
+++ src/Components/NavbarItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NavbarItem() {
+  return (
+      <button></button>
+  )
+}
+
+export default NavbarItem;
